2078edf4ba7baa54cba6242a772a5d78843a60b2
Rebecca Kurup Buchholz <rebecca.kurup@wsl.ch>
2022-08-25 15:31:24 +0200

    Test file for Record class and converters

---
+++ envidat/converters/test.py
@@ -0,0 +1,9 @@
+
+from envidat.metadata import Record
+from envidat.api.v1 import get_package
+
+test_package = get_package('number_of_forest_plots-125')
+record1 = Record(input_data=test_package, extract='iso')
+
+result_iso = record1.to_iso()
+# print(result_iso)
