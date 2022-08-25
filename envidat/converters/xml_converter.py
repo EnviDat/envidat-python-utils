1c265cd45b1724172d45bb7c4ad0a3a520699217
Rebecca Kurup Buchholz <rebecca.kurup@wsl.ch>
2022-08-25 15:07:24 +0200

    Refactored and renamed xml_converter.py

--- envidat/converters/xml_converter.py
+++ envidat/converters/xml_converter.py
@@ -9,7 +9,27 @@ from envidat.api.v1 import get_metadata_json_with_resources
 log = logging.getLogger(__name__)
 
 
-def envidat_to_xml_converter() -> str:
+def convert_xml(package: dict) -> str:
+    """
+    Converter function for EnviDat record to XML format.
+
+    :return: XML file
+
+    """
+
+    root = {'root': package}
+
+    # Try to convert packages (i.e. records with metadata) to XML format
+    try:
+        converted_data_xml = unparse(root, short_empty_elements=True, pretty=True)
+        return converted_data_xml
+
+    except Exception as e:
+        log.error("ERROR: Cannot convert EnviDat packages to XML format.")
+        log.error(e)
+
+
+def convert_xml_all_resources() -> str:
     """
     Converter function for EnviDat JSON records to XML format. JSON --> XML.
 
