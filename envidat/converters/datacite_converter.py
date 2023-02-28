d5801080cb7a489edf48fb8616a92b2420b934c5
Rebecca Kurup Buchholz <rebecca.kurup@wsl.ch>
2023-02-28 16:19:40 +0100

    feat: finish "affiliation" tags helper funciton in datacite converter

--- envidat/converters/datacite_converter.py
+++ envidat/converters/datacite_converter.py
@@ -69,7 +69,7 @@ def get_config_datacite_converter(
 # TODO investigate if any additional fields from EnviDat schema can be matched
 #  to Datacite schema, for example custom fields
 # TODO write docstring
-# TODO map "affiliation" for "Contributor" and "Creator" tags
+# TODO review EnviDat packages from "organization"
 def datacite_convert_dataset(dataset: dict, config: dict):
     """Convert EnviDat metadata package from CKAN to DataCite XML."""
 
@@ -408,6 +408,7 @@ def datacite_convert_dataset(dataset: dict, config: dict):
     return dc
 
 
+# TODO research if author email can/should be included
 def get_dc_creator(author: dict, config: dict):
     """Returns author information in DataCite "creator" tag format"""
 
@@ -435,80 +436,23 @@ def get_dc_creator(author: dict, config: dict):
     affiliations = []
     affiliation = author.get(config[dc_creator_tag]["affiliation"], "")
     if affiliation:
-        # TODO test calling affiliation_to_dc()
         aff = affiliation_to_dc(affiliation, config)
-        # print(aff)
-        affiliations += [{"#text": affiliation.strip()}]
+        affiliations += [aff]
 
     affiliation_02 = author.get("affiliation_02", "")
     if affiliation_02:
-        # TODO test calling affiliation_to_dc()
         aff_02 = affiliation_to_dc(affiliation_02, config)
-        # print(aff_02)
-        affiliations += [{"#text": affiliation_02.strip()}]
+        affiliations += [aff_02]
 
     affiliation_03 = author.get("affiliation_03", "")
     if affiliation_03:
-        # TODO test calling affiliation_to_dc()
         aff_03 = affiliation_to_dc(affiliation_03, config)
-        # print(aff_03)
-        affiliations += [{"#text": affiliation_03.strip()}]
+        affiliations += [aff_03]
 
     if affiliations:
         dc_creator["affiliation"] = affiliations
 
     return dc_creator
-
-
-# TODO finish function
-# TODO check names
-# TODO finish "affiliation" config in "config_converters.json"
-def affiliation_to_dc(affiliation, config):
-    """Returns affiliation in DataCite "affiliation" tag format.
-       Uses config to map commonly used affiliations in EnviDat packages
-       (i.e. "WSL", "SLF") with long names of instiutions
-       and ROR identifiers when available.
-    """
-
-    # Get key from config that corresponds to affiliation
-    aff_keys = {
-        "WSL": "wsl",
-        "Swiss Federal Institute for Forest, Snow and Landscape Research WSL": "wsl",
-        "WSL Swiss Federal Research Institute, Birmensdorf, Switzerland": "wsl",
-        "SLF": "slf",
-        "WSL Institute for Snow and Avalanche Research SLF, Davos Dorf, Switzerland":
-            "slf",
-        "WSL Institute for Snow and Avalanche Research SLF": "slf",
-        "ETH": "eth",
-        "ETHZ": "eth",  # TODO check this
-        "Eawag": "eawag",
-        "EPFL": "epfl",
-        "IAP": "iap",
-        "PSI": "psi",
-        "PSI, Paul Scherrer Institute, Villigen": "psi",
-        # TODO check this
-        "UZH": "uzh",
-        "University of Zurich": "uzh",
-        "University of Zürich": "uzh",
-        "TROPOS": "tropos",
-        "UNIL": "unil"
-    }
-
-    # TODO check if aff_key in config, else return default
-    # Get affiliation config
-    aff_config = config["affiliation"]
-
-    # Strip whitespace from affiliation argument
-    aff = affiliation.strip()
-
-    # Get org dictionary if it exists in config
-    aff_key = aff_keys.get(aff, "")
-    org = aff_config.get(aff_key, {})
-
-    if org:
-        return org
-
-    return {"#text": aff}
 
 
 def get_dc_contributor(maintainer: dict, config: dict):
@@ -551,8 +495,10 @@ def get_dc_contributor(maintainer: dict, config: dict):
     contributor_affiliation = maintainer.get(
         config[dc_contributor_tag]["affiliation"], ""
     )
+
     if contributor_affiliation:
-        dc_contributor["affiliation"] = contributor_affiliation.strip()
+        dc_contributor["affiliation"] = \
+            affiliation_to_dc(contributor_affiliation, config)
 
     contributor_type = maintainer.get(
         join_tags([dc_contributor_tag, "contributorType"]), "ContactPerson"
@@ -562,6 +508,52 @@ def get_dc_contributor(maintainer: dict, config: dict):
     )
 
     return dc_contributor
+
+
+def affiliation_to_dc(affiliation, config):
+    """Returns affiliation in DataCite "affiliation" tag format.
+       Uses config to map commonly used affiliations in EnviDat packages
+       (i.e. "WSL", "SLF") with long names of instiutions
+       and ROR identifiers when available.
+    """
+
+    # Get key from config that corresponds to affiliation
+    aff_keys = {
+        "WSL": "wsl",
+        "Swiss Federal Institute for Forest, Snow and Landscape Research WSL": "wsl",
+        "WSL Swiss Federal Research Institute, Birmensdorf, Switzerland": "wsl",
+        "SLF": "slf",
+        "WSL Institute for Snow and Avalanche Research SLF, Davos Dorf, Switzerland":
+            "slf",
+        "WSL Institute for Snow and Avalanche Research SLF": "slf",
+        "ETH": "eth",
+        "ETHZ": "eth",
+        "UZH": "uzh",
+        "University of Zurich": "uzh",
+        "University of Zürich": "uzh",
+        "EPFL": "epfl",
+        "EPFL, Lausanne Swiss Federal Institute of Technology, Lausanne and Sion":
+            "epfl",
+        "PSI": "psi",
+        "PSI, Paul Scherrer Institute, Villigen": "psi",
+        "IAP": "iap",
+        "TROPOS": "tropos",
+        "UNIL": "unil"
+    }
+
+    # Get affiliation config
+    aff_config = config["affiliation"]
+
+    # Strip whitespace from affiliation
+    aff = affiliation.strip()
+
+    # Return org dictionary if it exists in config
+    aff_key = aff_keys.get(aff, "")
+    org = aff_config.get(aff_key, {})
+    if org:
+        return org
+    # Else return only affiliation
+    return {"#text": aff}
 
 
 def get_dc_research_group(organization_title):
