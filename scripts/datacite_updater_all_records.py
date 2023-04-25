6a95112ba4151122962c6697c013321522d4949d
Rebecca Kurup Buchholz <rebecca.kurup@wsl.ch>
2023-04-25 16:34:43 +0200

    refactor: rename script to update all records with DataCite

--- scripts/datacite_updater_all_records.py
+++ scripts/datacite_updater_all_records.py
@@ -3,10 +3,10 @@ import time
 
 from envidat.doi.datacite_updater import datacite_update_all_records
 
-# COMMAND to run script:   python -m scripts.datacite_importer
+# COMMAND to run script:   python -m scripts.datacite_importer_all_records
 
 # Assign start_time
-print("Starting datacite_importer.py....")
+print("Starting datacite_updater_all_records.py....")
 start_time = time.time()
 
 # Update all EnviDat records on DataCite
@@ -16,7 +16,7 @@ datacite_update_all_records()
 # Assign and format timer, print execution time
 end_time = time.time()
 timer = end_time - start_time
-print(f"Ending datacite_importer.py, that took {round(timer, 3)} seconds")
+print(f"Ending datacite_updater_all_records.py, that took {round(timer, 3)} seconds")
 
 
 
