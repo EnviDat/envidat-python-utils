d2b62fa64eefab2a5b5e792942d80531923f830d
Rebecca Kurup Buchholz <rebecca.kurup@wsl.ch>
2023-04-26 12:29:28 +0200

    refactor: update comments in datacite_updater_all_records.py

--- scripts/datacite_updater_all_records.py
+++ scripts/datacite_updater_all_records.py
@@ -1,9 +1,9 @@
+# Script to update ALL Envidat records on datacite.
+# Command to run script:   python -m scripts.datacite_importer_all_records
 
 import time
-
 from envidat.doi.datacite_updater import datacite_update_all_records
 
-# COMMAND to run script:   python -m scripts.datacite_importer_all_records
 
 # Assign start_time
 print("Starting datacite_updater_all_records.py....")
